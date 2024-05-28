9349db4f52fee6e72d0135c1b776937a4a6c546a
NEREN <82198306+Nerenchek0426@users.noreply.github.com>
2024-05-28 14:14:12 +0000

    Validation Form

--- appp/src/components/registr.jsx
+++ appp/src/components/registr.jsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./styles/registr.css"
 
 const Mainform = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passwordDirty, setPasswordDirty] = useState(false)
+    const [emailError, setEmailError] = useState('Email не может быть пустым')
+    const [passwordError, setPasswordError] = useState('Пароль не может быть пустым')
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Некорректный Email')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        if (e.target.value.length < 3 || e.target.value.length > 15) {
+            setPasswordError('Пароль должен содержать не меньше 3 и не более 15 символов')
+            if (!e.target.value) {
+                setPasswordError('Пароль не может быть пустым')
+            }
+        } 
+        else {
+            setPasswordError('')
+        }
+    }
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPasswordDirty(true)
+                break
+        }
+    }
+
+
         return(
             <div className="app">
                 <form>
                     <h1>Регистрация</h1>
-                    <input name='email' type='text' placeholder='Enter your email....'/>
-                    <input name='password' type='password' placeholder='Enter your password....'/>
+                    <div className="inputs">
+                        {(emailDirty && emailError) && <div style={{color:'red'}}>{emailError}</div>}
+                        <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name='email' type='text' placeholder='Enter your email....'/>
+                        {(passwordDirty && passwordError) && <div style={{color:'red'}}>{passwordError}</div>}
+                        <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name='password' type='password' placeholder='Enter your password....'/>
+                    </div>
                     <button type='submit'>Registration</button>
                 </form>
             </div>
