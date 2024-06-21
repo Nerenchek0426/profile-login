1be2478061c16e6efe3cf8184cb7248b4af2c229
Qub41k <lavrinov.dima.456@gmail.com>
2024-06-21 10:55:02 +0000

    latest

--- reg/src/components/Upbar.jsx
+++ reg/src/components/Upbar.jsx
@@ -1,9 +1,25 @@
+import { useLinks } from './LinksContext';
 import styles from './styles/upbar.module.css';
+import { Link } from 'react-router-dom';
 
 const Upbar  = () => {
+    const {links, currentLink} = useLinks()
+
+    const visibleLinks = links.find(link => link.url === currentLink)
+
+    console.log(links, currentLink, visibleLinks)
     return (
         <div className={styles.header}>
-          
+            <ul>
+                {visibleLinks?.sublinks?.map(link => (
+                         <li>
+                            <Link to={link.url} style={{textDecoration: "none", color: "white", display: "flex", gap: "1em"}}>
+                               {link.icon}
+                               {link.name}
+                            </Link>
+                        </li>
+                    ))}         
+                </ul>
 
         </div>
     )
